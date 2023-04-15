522d1af7e3a0c5e5ccd7168eb209dd753cfdb9ed
Harry <107435091+hazzaaclark@users.noreply.github.com>
2023-04-15 17:20:19 +0100

    Update defines.s

--- defines.s
+++ defines.s
@@ -1,12 +1,51 @@
+;----------------------------------------------------------
+;           MARIO KART WII SYSTEM DECLARATIONS
+;                 (C) HARRY CLARK 2023
+;----------------------------------------------------------
 
-
+;----------------------------------------------------------
+;      THIS FILE IS FOR DEFINING THE FUNCTIONALITY
+;               FOR THE IBM BROADWAY CPU
+;      THE SAME CPU FOUND IN THE GC AND WII
+;----------------------------------------------------------
 
 .SECTION: DEFINES
 
 .DEFINES:
+
+;----------------------------------------------------------
+;      IBM BROADWAY'S 32 BIT GENERAL PURPOSE REGISTERS
+;----------------------------------------------------------
 
 R0 EQU 0
 R1 EQU 1
 R2 EQU 2
 R3 EQU 3
 R4 EQU 4
+R5 EQU 5
+R6 EQU 6
+R7 EQU 7
+R8 EQU 8
+R9 EQU 9
+R10 EQU 10
+R11 EQU 11
+R12 EQU 12
+R13 EQU 13
+R14 EQU 14
+R15 EQU 15
+R16 EQU 16
+R17 EQU 17
+R18 EQU 18
+R19 EQU 19
+R20 EQU 20
+R21 EQU 21
+R22 EQU 22
+R23 EQU 23
+R24 EQU 24
+R25 EQU 25
+R26 EQU 26
+R27 EQU 27
+R28 EQU 28
+R29 EQU 29
+R30 EQU 30
+R31 EQU 31
